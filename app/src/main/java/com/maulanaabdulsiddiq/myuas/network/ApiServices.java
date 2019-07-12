410e5b7353a9a5bf765df3a11e1b5f157bf234f9
Maulana Abdul Siddiq <siddiqmaulanaabdul@gmail.com>
2019-07-12 23:03:16 +0700

    Initial commit

---
+++ app/src/main/java/com/maulanaabdulsiddiq/myuas/network/ApiServices.java
@@ -0,0 +1,16 @@
+package com.maulanaabdulsiddiq.myuas.network;
+
+import com.maulanaabdulsiddiq.myuas.response.ResponseBerita;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+
+public interface ApiServices {
+
+    //@TIPEMETHOD("API_END_POINT")
+    @GET("tampil_berita.php")
+    Call<ResponseBerita> request_show_all_berita();
+    // <ModelData> nama_method()
+
+}
